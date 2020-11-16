4e92e1a38721536a674d095f2d02ce39cd64ac8b
JustGinger888 <ethanmoffatt888@icloud.com>
2020-11-16 23:56:00 +0000

    lodash

---
+++ 01/asyncDemo.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+
+function phoneNumber (err, data) {
+    console.log('data:', data);
+}
+
+fs.readdir('c:/', phoneNumber);
+
+console.log('Comes After');
---
+++ 02/myModule.js
@@ -0,0 +1,1 @@
+exports.myText = "Module Says Hi";
