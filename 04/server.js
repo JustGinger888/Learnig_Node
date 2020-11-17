45a9b665e983e5b33aa79765d5792430ed2021d7
JustGinger888 <ethanmoffatt888@icloud.com>
2020-11-17 22:19:17 +0000

    ChatApp CMP

--- 04/server.js
+++ 04/server.js
@@ -3,10 +3,18 @@ const bodyParser = require('body-parser');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+const mongoose = require('mongoose');
 
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+
+var dbURL = "mongodb+srv://user:user@cluster0.0yyio.mongodb.net/LearningNode?retryWrites=true&w=majority";
+
+var Message = mongoose.model('Message', {
+    name: String,
+    message: String
+})
 
 var messages = [
     {name:'Ted', message: 'hello'},
@@ -14,18 +22,31 @@ var messages = [
 ]
 
 app.get('/messages', (req, res) => {
-    res.send(messages)
+    Message.find({}, (err, messages) => {
+        res.send(messages)
+    })
 })
 
 app.post('/messages', (req, res) => {
-    messages.push(req.body);
-    io.emit('message', req.body)
-    res.sendStatus(200);
+    var message = new Message(req.body)
+
+    message.save((err) => {
+        if (err) {
+            sendStatus(500);
+        } else {
+            io.emit('message', req.body)
+            res.sendStatus(200);
+        }
+    })
 })
 
 io.on('connection', (socket => {
     console.log("A User Connected");
 }))
+
+mongoose.connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
+    console.log("Mongo Connection", err);
+})
 
 var server = http.listen(8080, () => {
     console.log('Listening on port ', server.address().port);
