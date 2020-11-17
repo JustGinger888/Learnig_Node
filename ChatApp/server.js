a980a9bd42c3091b68d3ceb44ae5e9f1232146c1
JustGinger888 <ethanmoffatt888@icloud.com>
2020-11-17 22:48:36 +0000

    upd

--- ChatApp/server.js
+++ ChatApp/server.js
@@ -9,6 +9,8 @@ app.use(express.static(__dirname));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
+mongoose.Promise = Promise
+
 var dbURL = "mongodb+srv://user:user@cluster0.0yyio.mongodb.net/LearningNode?retryWrites=true&w=majority";
 
 var Message = mongoose.model('Message', {
@@ -16,28 +18,37 @@ var Message = mongoose.model('Message', {
     message: String
 })
 
-var messages = [
-    {name:'Ted', message: 'hello'},
-    {name:'Bill', message: 'hi'}
-]
-
 app.get('/messages', (req, res) => {
     Message.find({}, (err, messages) => {
         res.send(messages)
     })
 })
 
-app.post('/messages', (req, res) => {
-    var message = new Message(req.body)
+app.post('/messages', async (req, res) => {
+    try {
+        var message = new Message(req.body);
 
-    message.save((err) => {
-        if (err) {
-            sendStatus(500);
-        } else {
+        var savedMessage = await message.save();
+
+        console.log('saved');
+
+        var censored = await Message.findOne({ message: 'badword' });
+
+        if (censored) 
+            await Message.remove({ _id: censored.id })
+        else
             io.emit('message', req.body)
-            res.sendStatus(200);
-        }
-    })
+
+        res.sendStatus(200)
+    } catch (error) {
+        res.sendStatus(500);
+        return console.error(error);
+    } finally {
+        console.log("Post Called");
+    }
+
+        
+
 })
 
 io.on('connection', (socket => {
